aa901b2afb36b0c69435856327bc9137e02d378d
oNePlay <carnificem1993@gmail.com>
2024-03-03 20:27:14 +0300

    начальный коммит

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    String name;
+    String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getNameAuthor() {
+        return name;
+    }
+
+    public String getSurnameAuthor() {
+        return surname;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ public class Main {
 
 
     public static void main(String[] args) {
+        Author viktorPelevin = new Author("Виктор ", "Пелевин ");
+        Author grigoriyKlimov = new Author("Григорий ", "Климов");
+
+        book generationP = new book("Generation\"П\" ", viktorPelevin, 1999);
+        book myNameIsLegion = new book("Имя мне легион", grigoriyKlimov, 1975);
+
+        System.out.println(" Автор: " + generationP.getAuthor().getNameAuthor() + " " + generationP.getAuthor().getSurnameAuthor() +
+                ",название:" + generationP.getTitle() + ",год издания: " + generationP.getYearPublication());
+        System.out.println(" Автор: " + myNameIsLegion.getAuthor().getNameAuthor() + " " + myNameIsLegion.getAuthor().getSurnameAuthor() +
+                ",название:" + myNameIsLegion.getTitle() + ",год издания: " + myNameIsLegion.getYearPublication());
 
     }
 }
